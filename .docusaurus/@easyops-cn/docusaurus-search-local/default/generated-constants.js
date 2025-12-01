83a93d9f11bf46274dad72ef596bb6128ce0ffb8
dziedom <dziedzom@gmail.com>
2025-12-01 10:36:36 +0000

    Upgrade search plugin to v0.52.2, fix Vercel build, add .npmrc, update content

---
+++ .docusaurus/@easyops-cn/docusaurus-search-local/default/generated-constants.js
@@ -0,0 +1,6 @@
+import lunr from "C:\\SigmaGrid\\sigmagrid-docs\\node_modules\\lunr\\lunr.js";
+export const removeDefaultStopWordFilter = [];
+export const language = ["en"];
+export const searchIndexUrl = "search-index{dir}.json?_=a748f408";
+export const searchResultLimits = 8;
+export const fuzzyMatchingDistance = 1;
